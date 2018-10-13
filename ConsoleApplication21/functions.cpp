6676aa591b34672f7387f614ae96bff094353e15
Mariusz <mariuszpisarski0@gmail.com>
2018-10-13 09:51:28 +0200

    first

---
+++ ConsoleApplication21/functions.cpp
@@ -0,0 +1,30 @@
+#include "CImg.h"
+#include "pch.h"
+#include "source.h"
+#include <iostream>
+#include <string>
+using namespace std;
+using namespace cimg_library;
+void Help()
+{
+	cout << endl << "[ProgramName] [Picturename] --SomeOption" << endl;
+	cout << "Program that implements a picture and changes it" << endl;
+	cout << "Possible options:" << endl;
+	cout << "--We_Have_None_Up_Until_Now" << endl << endl;
+}
+
+CImg<double> Brightness(CImg<double> image, int x)
+{
+	for (int i = 0; i < image.width(); i++)
+	{
+		for (int j = 0; j < image.height(); j++)
+		{
+			for (int k = 0; k < 3; k++)
+			{
+				if ((image(i, j, k) + x) >= 255) image(i, j, k) = 255;
+				else image(i, j, k) += x;
+			}
+		}
+	}
+	return image;
+}
---
+++ ConsoleApplication21/source.h
@@ -0,0 +1,10 @@
+#include "CImg.h"
+#include "pch.h"
+#include <iostream>
+#include <string>
+using namespace std;
+using namespace cimg_library;
+
+extern void Help();
+
+CImg<double> Brightness(CImg<double> image, int x);
