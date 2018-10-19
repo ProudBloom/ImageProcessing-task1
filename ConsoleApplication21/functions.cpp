60ae85acd6bd82cc070e93c73029467d4f1b66a7
ProudBloom <jakub-sztompka@wp.pl>
2018-10-19 16:14:06 +0200

    Code fixed

--- ConsoleApplication21/functions.cpp
+++ ConsoleApplication21/functions.cpp
@@ -8,58 +8,31 @@ using namespace cimg_library;
 
 void Help()
 {
-<<<<<<< HEAD
-	cout << endl;
-	cout << "================HELP MENU================" << endl;
-	cout << "Following program loads a picture and changes it in a desired way." << endl;
-	cout << "Syntax: <ProgramName> <InputPictureName> --SomeOption <value> <OutputPictureName>" << endl << endl;
-	cout << "Possible options:" << endl
-		<< "--brightness <value>" << endl
-=======
 	cout << "=====HELP MENU=====\n" << endl;
 	cout << "Following program implements a picture and changes it in a desired way.\n" << endl;
 	cout << "Syntax : " << endl;
 	cout << endl << "<ProgramName> <InputPictureName> --SomeOption <value> <OutputPictureName>\n" << endl;
 	cout << "Possible options:\n" << endl;
 	cout << "--brightness <value>" << endl
->>>>>>> 99e4f3d19d03f733c6628da7e05810bb5ac45eba
 		<< "--negative" << endl
 		<< "--contrast <value>" << endl
 		<< "--hflip" << endl
 		<< "--vflip" << endl
-<<<<<<< HEAD
 		<< "--dflip" << endl << endl;
 	cout << "ImgProc Application made by Mariusz Pisarski & Jakub Sztompka IT" << endl << "Ver. 0.1" << endl;
-=======
-		<< "--dflip" << endl;
->>>>>>> 99e4f3d19d03f733c6628da7e05810bb5ac45eba
 }
 
 CImg <double> Brightness(CImg<double> image, int value)
 {
-	
-
 	for (int i = 0; i < image.width(); i++)
 	{
 		for (int j = 0; j < image.height(); j++)
 		{
 			for (int k = 0; k < image.spectrum(); k++)
 			{
-<<<<<<< HEAD
 				image(i, j, k) += value;
 				if ((image(i, j, k)) > 255) image(i, j, k) = 255;
 				else if ((image(i, j, k)) < 0) image(i, j, k) = 0;
-=======
-				if ((image(i, j, k) + value) >= 255)
-				{
-					image(i, j, k) = 255;
-				}
-				else if ((image(i, j, k) + value) <= 0) 
-				{
-					image(i, j, k) = 0;
-				}
-				else image(i, j, k) += value;
->>>>>>> 99e4f3d19d03f733c6628da7e05810bb5ac45eba
 			}
 		}
 	}
@@ -81,15 +54,9 @@ CImg <double> Negative(CImg<double> image)
 	return image;
 }
 
-<<<<<<< HEAD
 CImg <double> Contrast(CImg<double> image, int value)
 {
-	//float cont_factor;
-=======
-CImg <double> Contrast(CImg<double> image, float value)
-{
 	float cont_factor;
->>>>>>> 99e4f3d19d03f733c6628da7e05810bb5ac45eba
 
 	for (int i = 0; i < image.width(); i++)
 	{
@@ -97,13 +64,8 @@ CImg <double> Contrast(CImg<double> image, float value)
 		{
 			for (int k = 0; k < image.spectrum(); k++)
 			{
-<<<<<<< HEAD
 				//cont_factor = (259 * (value + 255)) / (255 * (259 - value));
 				image(i, j, k) = ((image(i, j, k) - 127) * value) + 127;
-=======
-				cont_factor = (259 * (value + 255)) / (255 * (259 - value));
-				image(i, j, k) = ((image(i, j, k) - 127) * cont_factor) + 127;
->>>>>>> 99e4f3d19d03f733c6628da7e05810bb5ac45eba
 			}
 		}
 	}
@@ -112,7 +74,6 @@ CImg <double> Contrast(CImg<double> image, float value)
 
 CImg <double> HorizontalFlip(CImg <double> image)
 {
-<<<<<<< HEAD
 	for (int i = 0; i < image.width() / 2; i++)
 	{
 		for (int j = 0; j < image.height(); j++)
@@ -123,24 +84,13 @@ CImg <double> HorizontalFlip(CImg <double> image)
 
 				image(i, j, k) = image((image.width() - 1) - i, j, k);
 				image((image.width() - 1) - i, j, k) = temp;
-=======
-	for (int i = 0; i < (image.width()/2); i++)
-	{
-		for (int j = 0; j < image.height(); j++)
-		{
-			for (int k = 0; k < 3; k++)
-			{
-				double temp = image(i, j, k);
-				image(i, j, k) = image( (image.width()-1) - i, j, k);
-				image( (image.width() - 1) - i, j, k) = temp;
->>>>>>> 99e4f3d19d03f733c6628da7e05810bb5ac45eba
 			}
 		}
 	}
+
 	return image;
 }
 
-<<<<<<< HEAD
 CImg <double> VerticalFlip(CImg <double> image)
 {
 	for (int i = 0; i < image.width(); i++)
@@ -153,19 +103,6 @@ CImg <double> VerticalFlip(CImg <double> image)
 
 				image(i, j, k) = image(i, (image.height() - 1) - j, k);
 				image(i,(image.height() - 1) - j, k) = temp;
-=======
-CImg <double> VercicalFlip(CImg <double> image)
-{
-	for (int i = 0; i < image.width(); i++)
-	{
-		for (int j = 0; j < (image.height()/2); j++)
-		{
-			for (int k = 0; k < 3; k++)
-			{
-				double temp = image(i, j, k);
-				image(i, j, k) = image(i, (image.height() - 1) - j, k);
-				image (i, (image.height() - 1) - j, k) = temp;
->>>>>>> 99e4f3d19d03f733c6628da7e05810bb5ac45eba
 			}
 		}
 	}
@@ -174,7 +111,6 @@ CImg <double> VercicalFlip(CImg <double> image)
 
 CImg <double> DiagonalFlip(CImg <double> image)
 {
-<<<<<<< HEAD
 	for (int i = 0; i < image.width(); i++)
 	{
 		for (int j = 0; j < image.height() / 2; j++)
@@ -190,8 +126,4 @@ CImg <double> DiagonalFlip(CImg <double> image)
 	}
 	return image;
 }
-=======
-	return  HorizontalFlip(VercicalFlip(image));
-}
 
->>>>>>> 99e4f3d19d03f733c6628da7e05810bb5ac45eba
